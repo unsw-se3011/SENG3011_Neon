24a440d56dc5c622e1874447c2f94784a0ea1bbc
Toby HUANG <619058900@qq.com>
2019-02-25 16:49:05 +1100

    grap all the source post from host

---
+++ scraper/ProMed/second.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat 1.data 15.data 17.data 24.data | sort -r | uniq > all.data
--- scraper/scraper/spiders/promed.py
+++ scraper/scraper/spiders/promed.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import fileinput
 
+def get_url(text):
+    return "http://www.promedmail.org/post/%s" % text 
 
 class PromedSpider(scrapy.Spider):
     name = 'promed'
     allowed_domains = ['http://www.promedmail.org']
-    start_urls = ['http://http://www.promedmail.org/']
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
