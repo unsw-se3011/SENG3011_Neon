a36d8ded74ceb9572bfe67ec2ba47f4ab60593b2
zzzsoftware <30430450+zzzsoftware@users.noreply.github.com>
2019-02-28 11:33:19 +1100

    tokenize nltk oubreak.jl

--- scraper/testing.py
+++ scraper/testing.py
@@ -10,30 +10,46 @@ from nltk.stem import *
 
 
 
-sentence = "Hello, this is a test for nltk testing"
-s1 = "This is a sample sentence, showing off the stop words filtration."
-stop_words = set(stopwords.words('english'))
+# from nltk.tokenize import sent_tokenize, PunktSentenceTokenizer
+# from nltk.corpus import gutenberg
+# from json import loads
 
 
-filtered_sentence = []
+# sample text
+sample = gutenberg.raw("/Users/iriszhang/Desktop/ProjectNeon-w1-nltk/scraper/oubreak.jl")
 
-for w in word_tokenize(s1):
-    if w not in stop_words:
-        filtered_sentence.append(w)
+tok = sent_tokenize(sample)
 
-print(filtered_sentence)
+for x in range(5):
+    print(tok[x])
+    print( '\n')
 
 
 
-filtered_sentence = []
+# sentence = "Hello, this is a test for nltk testing"
+# s1 = "This is a sample sentence, showing off the stop words filtration."
+# stop_words = set(stopwords.words('english'))
 
-new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
-ps = PorterStemmer()
 
-for w in word_tokenize(new_text):
-    filtered_sentence.append(ps.stem(w))
+# filtered_sentence = []
 
-print(filtered_sentence)
+# for w in word_tokenize(s1):
+#     if w not in stop_words:
+#         filtered_sentence.append(w)
+
+# print(filtered_sentence)
+
+
+
+# filtered_sentence = []
+
+# new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
+# ps = PorterStemmer()
+
+# for w in word_tokenize(new_text):
+#     filtered_sentence.append(ps.stem(w))
+
+# print(filtered_sentence)
 
 
 # print(sent_tokenize(sentence))
