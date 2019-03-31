aa0506e621447a0b04745c80bb99d2c1e598db24
littlerest1 <891982668yzq@gmail.com>
2019-04-01 00:02:33 +1100

    add more test
    without post/get user with json object requesting. For user registration and login maybe we need to use Jmeter for test

--- testReport.sh
+++ testReport.sh
@@ -6,7 +6,7 @@ fi
 
 if [ ! -f format.txt ]; then
     touch format.txt
-	echo "%{time_total}" >> format.txt
+	echo "%{time_total} " >> format.txt
 	printf "\n" >> format.txt
 fi
 
@@ -29,4 +29,90 @@ times="10"
 ave=$(echo "$total / $times" | bc)
 printf 'average time for requested 10 times from reports endpoint %.3f s\n' "$(echo "scale=8;$total/10" | bc)"
 
-rm output.txt
+ > output.txt
+
+time=0
+while [  $time -lt 10 ]; do
+	curl -w "@format.txt" -o /dev/null -s "http://localhost:8000/v0/articles/" >> output.txt
+	printf "\n" >> output.txt
+	let time=time+1 
+done
+
+total="0"
+file="output.txt"
+while IFS= read line
+do
+	total=$(echo "scale=3; $total + $line" | bc)
+done <"$file"
+
+times="10"
+ave=$(echo "$total / $times" | bc)
+printf 'average time for requested 10 times from articles endpoint %.3f s\n' "$(echo "scale=8;$total/10" | bc)"
+
+ > output.txt
+ 
+time=0
+while [  $time -lt 10 ]; do
+	curl -w "@format.txt" -o /dev/null -s "http://localhost:8000/v0/location/" >> output.txt
+	printf "\n" >> output.txt
+	let time=time+1 
+done
+
+total="0"
+file="output.txt"
+while IFS= read line
+do
+	total=$(echo "scale=3; $total + $line" | bc)
+done <"$file"
+
+times="10"
+ave=$(echo "$total / $times" | bc)
+printf 'average time for requested 10 times from location endpoint %.3f s\n' "$(echo "scale=8;$total/10" | bc)"
+
+ > output.txt
+ 
+ time=0
+while [  $time -lt 10 ]; do
+	curl -w "@format.txt" -o /dev/null -s "http://localhost:8000/v0/diseases/" >> output.txt
+	printf "\n" >> output.txt
+	let time=time+1 
+done
+
+total="0"
+file="output.txt"
+while IFS= read line
+do
+	total=$(echo "scale=3; $total + $line" | bc)
+done <"$file"
+
+times="10"
+ave=$(echo "$total / $times" | bc)
+printf 'average time for requested 10 times from disease endpoint %.3f s\n' "$(echo "scale=8;$total/10" | bc)"
+ 
+  > output.txt
+  
+ time=0
+while [  $time -lt 10 ]; do
+	curl -w "@format.txt" -o /dev/null -s "http://localhost:8000/v0/syndromes/" >> output.txt
+	printf "\n" >> output.txt
+	let time=time+1 
+done
+
+total="0"
+file="output.txt"
+while IFS= read line
+do
+	total=$(echo "scale=3; $total + $line" | bc)
+done <"$file"
+
+times="10"
+ave=$(echo "$total / $times" | bc)
+printf 'average time for requested 10 times from syndromes endpoint %.3f s\n' "$(echo "scale=8;$total/10" | bc)"
+ 
+ > output.txt
+
+
+
+ 
+ 
+ 
