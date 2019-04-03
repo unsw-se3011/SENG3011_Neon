247d684b2fb8f3e0c155db5b37d2ff1424decc78
XINZESONG <z5092119@ad.unsw.edu.au>
2019-04-04 03:58:01 +1100

    Get more disease match result
    Add the Disease map and change the keys to noun with new map. and change the match disease fun.

--- PHASE_1/importer/article/nlpe.py
+++ PHASE_1/importer/article/nlpe.py
@@ -29,6 +29,135 @@ COUNTRY_LIST = [
     'Afghanistan','Albania','Algeria','Andorra','Angola','Anguilla','Antigua &amp; Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia','Bosnia &amp; Herzegovina','Botswana','Brazil','British Virgin Islands','Brunei','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Cape Verde','Cayman Islands','Chad','Chile','China','Colombia','Congo','Cook Islands','Costa Rica','Cote D Ivoire','Croatia','Cruise Ship','Cuba','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Estonia','Ethiopia','Falkland Islands','Faroe Islands','Fiji','Finland','France','French Polynesia','French West Indies','Gabon','Gambia','Georgia','Germany','Ghana','Gibraltar','Greece','Greenland','Grenada','Guam','Guatemala','Guernsey','Guinea','Guinea Bissau','Guyana','Haiti','Honduras','Hong Kong','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Isle of Man','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kuwait','Kyrgyz Republic','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Macau','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Mauritania','Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Montserrat','Morocco','Mozambique','Namibia','Nepal','Netherlands','Netherlands Antilles','New Caledonia','New Zealand','Nicaragua','Niger','Nigeria','Norway','Oman','Pakistan','Palestine','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Puerto Rico','Qatar','Reunion','Romania','Russia','Rwanda','Saint Pierre &amp; Miquelon','Samoa','San Marino','Satellite','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','South Africa','South Korea','Spain','Sri Lanka','St Kitts &amp; Nevis','St Lucia','St Vincent','St. Lucia','Sudan','Suriname','Swaziland','Sweden','Switzerland','Syria','Taiwan','Tajikistan','Tanzania','Thailand','Timor LEste','Togo','Tonga','Trinidad &amp; Tobago','Tunisia','Turkey','Turkmenistan','Turks &amp; Caicos','Uganda','Ukraine','United Arab Emirates','United Kingdom','Uruguay','Uzbekistan','Venezuela','Vietnam','Virgin Islands (US)','Yemen','Zambia','Zimbabwe'
 ]
 
+DISEASE_NOUN = {
+    "unknown": "unknown",
+    "other": "other",
+    "cutaneous": "anthrax cutaneous",
+    "gastrointestinous": "anthrax gastrointestinous",
+    "inhalation": "anthrax inhalation",
+    "botulism": "botulism",
+    "Food-borne": "botulism",
+    "Intestinal": "botulism",
+    "brucellosis": "brucellosis",
+    "Brucella": "brucellosis",
+    "chikungunya": "chikungunya",
+    "cholera": "cholera",
+    "cryptococcosis": "cryptococcosis",
+    "cryptosporidiosis": "cryptosporidiosis",
+    "crimean-congo": "crimean-congo haemorrhagic fever",
+    "dengue": "dengue",
+    "diphtheria": "diphteria",
+    "EVD": "ebola haemorrhagic fever",
+    "ebola": "ebola haemorrhagic fever",
+    "EHEC": "ehec (e.coli)",
+    "Escherichia": "ehec(e.coli)",
+    "e.coli": "ehec(e.coli)",
+    "EV71": "enterovirus 71 infection",
+    "enterovirus": "enterovirus 71 infection",
+    "a/h5n1": "influenza a/h5n1",
+    "a/h7n9": "influenza a/h7n9",
+    "a/h9n2": "influenza a/h9n2",
+    "a/h1n1": "influenza a/h1n1",
+    "a/h1n2": "influenza a/h1n2",
+    "a/h3n5": "influenza a/h3n5",
+    "a/h3n2": "influenza a/h3n2",
+    "a/h2n2": "influenza a/h2n2",
+    "HFMD": "hand, foot and mouth disease",
+    "HPS": "hantavirus",
+    "hantavirus": "hantavirus",
+    # "hepatitis a":"hepatitis",
+    # "hepatitis b":"hepatitis",
+    # "hepatitis c":"hepatitis",
+    # "hepatitis d":"hepatitis",
+    # "hepatitis e":"hepatitis",
+    "Histoplasma": "histoplasmosis",
+    "reticuloendotheliosis": "histoplasmosis",
+    "histoplasmosis": "histoplasmosis",
+    "HIV": "hiv/aids",
+    "AIDS": "hiv/aids",
+    "lassa": "lassa fever",
+    "Malaria": "malaria",
+    "MVD": "marburg virus disease",
+    "MARV": "marburg virus disease",
+    "RAVV": "marburg virus disease",
+    "measles": "measles",
+    "MERS": "mers-cov",
+    "MERS-coronavirus": "mers-cov",
+    "mumps": "mumps",
+    "RNA": "nipah virus",
+    "Henipavirus": "nipah virus",
+    "Noroviruses": "norovirus infection",
+    "Norwalk-like": "norovirus infection",
+    "Pertussis": "pertussis",
+    "whoop": "pertussis",
+    "plague": "plague",
+    "Yersinia": "plague",
+    "Streptococcus": "pneumococcus pneumonia",
+    "Pneumococcal": "pneumococcus pneumonia",
+    "polio": "poliomyelitis",
+    "poliomyelitis": "poliomyelitis",
+    # "Coxiella": "q fever",
+    "rabies": "rabies",
+    "RVF": "rift valley fever",
+    "Rotavirus": "rotavirus infection",
+    "rubella": "rubella",
+    "salmonellosis": "salmonellosis",
+    "Salmonella": "salmonellosis",
+    "SARS": "sars",
+    "Shigella": "shigellosis",
+    "Variola": "smallpox",
+    "smallpox": "smallpox",
+    "SEB": "staphylococcal enterotoxin b",
+    "enteric": "thypoid fever",
+    "Salmonella typhi": "thypoid fever",
+    "TB": "tuberculosis",
+    "MTB": "tuberculosis",
+    "Tularemia": "tularemia",
+    "tularensis": "tularemia",
+    "cowpox": "vaccinia and cowpox",
+    "vaccinia": "vaccinia and cowpox",
+    "varicella": "varicella",
+    "Chickenpox": "varicella",
+    "VZV": "varicella",
+    "WNV": "west nile virus",
+    "EYE": "yellow fever",
+    "Yersinia": "yersiniosis",
+    "yersiniosis": "yersiniosis",
+    "zika": "zika",
+    "Legionella": "legionares",
+    "legionares": "legionares",
+    "Listeria": "listeriosis",
+    "monkeypox": "monkeypox"
+}
+
+#Not sure how  to use this fun in class, re-define
+def get_wordnet_pos(tag):
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return None
+
+#import disease
+KEY_DISEASE = []
+VALUE_DISEASE = []
+for keys in DISEASE_NOUN:
+    KEY_DISEASE.append(keys)
+    VALUE_DISEASE.append(DISEASE_NOUN[keys])
+tagged_sent = pos_tag(KEY_DISEASE)
+WNL = WordNetLemmatizer()
+LEMMAS_DISEASE = []
+for tag in tagged_sent:
+    wordnet_pos = get_wordnet_pos(tag[1]) or wordnet.NOUN
+    LEMMAS_DISEASE.append(WNL.lemmatize(tag[0], pos=wordnet_pos))
+#get all noun_keys into new map with value
+MATCH_DISEASE = dict(zip(LEMMAS_DISEASE,VALUE_DISEASE))
+
 # import city to memory 
 CITIES = []
 with open('world-cities.csv', 'r') as csvfile:
@@ -47,8 +176,9 @@ class Nlpe(object):
         self.country_tags = self.country_text()
         self.people_tags = self.noun_text()
         self.date_tags = self.noun_text()
+        self.disease_tags = self.noun_text()
 
-    def get_wordnet_pos(self, tag):
+    def get_wordnet_pos(self,tag):
         if tag.startswith('J'):
             return wordnet.ADJ
         elif tag.startswith('V'):
@@ -58,8 +188,7 @@ class Nlpe(object):
         elif tag.startswith('R'):
             return wordnet.ADV
         else:
-            return None
-
+            return None 
     # convert all self.text to lowercase, delete stop words 
     def parse_pos_tags(self):
         sr = stopwords.words('english')
@@ -138,9 +267,11 @@ class Nlpe(object):
     def get_disease(self):
         # disease match
         disease1 = set()
-        for word, pos in self.pos_tags:
+        for word, pos in self.disease_tags:
             if word in DISEASE:
                 disease1.add(word)
+            if word in MATCH_DISEASE:
+                disease1.add(MATCH_DISEASE[word])
         return [t for t in disease1]
 
     def get_event(self):
@@ -183,7 +314,6 @@ class Nlpe(object):
                     'state': city_dict['state'],
                     'country': city_dict['country'],
                     'name': city_dict['state'],
-
                 }
             if city_dict['country'] in places.countries:
                 location_dict[city_dict['country']] = {
