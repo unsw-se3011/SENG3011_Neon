b7387bf98cf4a042fa8b319b110e2e77280b40ca
littlerest1 <891982668yzq@gmail.com>
2019-04-23 14:23:49 +1000

    finished PadeTrack

---
+++ PHASE_1/Spider/PadeTrack/locationSet.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+# import city to memory
+
+import csv
+from json import dumps
+CITIES = []
+with open('world-cities.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        row = list(row.items())
+        CITIES.append({
+            'city': row[0][1], 'country': row[1][1], 'state': row[2][1]
+        })
+
+
+class LocationSet(object):
+    def __init__(self):
+        # atomic location set
+        self.location_set = set()
+        # list of location object
+        self.location_list = []
+
+    def add_place_obj(self, place_obj: dict):
+        self.location_list.append(place_obj)
+        self.location_set.add(place_obj.get('country'))
+        self.location_set.add(place_obj.get('state'))
+        self.location_set.add(place_obj.get('city'))
+
+    def find_collapse(self, level, value):
+        # find at that level, which is collapse
+        for t in self.location_list:
+            if t.get(level) == value:
+                return t
+
+        # doesn't find the object
+        return None
+
+    def add(self, place: str):
+        place_obj = {}
+        for city_dict in CITIES:
+            low_city = city_dict['city']
+            low_country = city_dict['country']
+            low_state = city_dict['state']
+            place.lower()
+
+            # map the place string to place object
+            if place == low_city:
+                place_obj = {
+                    'country': city_dict['country'],
+                    'state': city_dict['state'],
+                    'city': city_dict['city'],
+                    'name': city_dict['city'],
+                }
+                break
+            elif place == low_state:
+                place_obj = {
+                    'country': city_dict['country'],
+                    'state': city_dict['state'],
+                    'name': city_dict['state'],
+                }
+                break
+            elif place == low_country:
+                place_obj = {
+                    'country': city_dict['country'],
+                    'name': city_dict['country'],
+                }
+                break
+
+        # print('imhere with' + place)
+        # print(place_obj)
+        if 'name' not in place_obj:
+            # cloudn't find the place in database
+            return False
+
+        if 'city' in place_obj and \
+                self.find_collapse('city', place_obj['city']):
+
+            # this object is recorded
+            return False
+        if 'state' in place_obj and\
+                self.find_collapse('state', place_obj['state']):
+                # get the collapse object
+            col = self.find_collapse('state', place_obj['state'])
+            if 'city' not in col:
+                # this place is more advanced
+                self.location_list = \
+                    [t for t in self.location_list if t != col]
+        elif self.find_collapse('country', place_obj['country']):
+            col = self.find_collapse('country', place_obj['country'])
+            if 'city' not in col and 'state' not in col:
+                # new place is more advanced
+                self.location_list = \
+                    [t for t in self.location_list if t != col]
+
+        self.add_place_obj(place_obj)
+        return True
+
+
+if __name__ == "__main__":
+    ls = LocationSet()
+    ls.add("Brazil")
+    ls.add("Brazilia")
+    print(ls.location_list)
--- PHASE_1/Spider/PadeTrack/main.py
+++ PHASE_1/Spider/PadeTrack/main.py
@@ -1,15 +1,81 @@
 #!/usr/bin/python3
 
 import requests
+import json
 from json import dumps
 import re
 import datetime
-# from locationSet import LocationSet
+from locationSet import LocationSet
 from math import ceil
 
 # http://api.pandetrack.online/listall/reports
 
 # "2019-07-01Txx:xx:xx"
+FUZZ_MAP = {
+    6: "Y",
+    5: "M",
+    4: "D",
+    3: "H",
+    2: "I",
+    1: "S",
+    0: "A"
+}
+
+
+PRESENCE = "P"
+DEATH = "D"
+INFECTED = "I"
+HOSPITALISED = "H"
+RECOVERED = "R"
+
+EVENT_TYPE_MAP = {
+    'presence': PRESENCE,
+    'death': DEATH,
+    'infected': INFECTED,
+    'hospitalised': HOSPITALISED,
+    'recovered': RECOVERED,
+}
+def dd(str):
+    print(str)
+    exit(0)
+    
+class FuzzDate(object):
+    def __init__(self, date_str):
+        self.date_str = date_str
+        res = re.search(
+            r'([0-9]{4})-([0-9x]{1,2})-([0-9x]{1,2})T([0-9x]{1,2}):([0-9x]{1,2}):([0-9x]{1,2})', self.date_str)
+        if res is not None:
+            self.date_group = [t for t in res.groups()]
+        else:
+            res = re.search(
+                r'([0-9]{4})-([0-9x]{1,2})-([0-9x]{1,2}) ([0-9x]{1,2}):([0-9x]{1,2}):([0-9x]{1,2})', self.date_str)
+            self.date_group = [t for t in res.groups()]
+        
+        # count how much field is fuzzy and replace it
+        self.fuzz_count = 0
+        new_date_group = []
+        index = 0
+        for t in self.date_group:
+            if t[0] == 'x':
+                self.fuzz_count += 1
+                # replace the t = 01
+                if index < 3:
+                    t = "01"
+                else:
+                    t = "00"
+            new_date_group.append(t)
+
+            index += 1
+        # replace the new date group
+        self.date_group = [int(t) for t in new_date_group]
+
+    def get_datetime(self):
+        # easy python list to function input
+        return datetime.datetime(*self.date_group).isoformat()
+
+    def get_fuzz_level(self):
+        return FUZZ_MAP[self.fuzz_count]
+
 
 class ReportParser(object):
     def __init__(self, report: dict):
@@ -19,12 +85,13 @@ class ReportParser(object):
         for t in self.report['reported_events']:
             temp_list += self.parseReportEvent(t)
         self.report['report_events'] = temp_list
+
         del self.report['reported_events']
-        # print(self.dumps())
+        print(self.dumps())
 
     def parseReportEvent(self, re):
         fd = FuzzDate(re['date'])
-
+       
         # dd(fd.get_fuzz_level())
         re['start_date'] = fd.get_datetime()
         re['start_date_fuzz'] = fd.get_fuzz_level()
@@ -36,9 +103,10 @@ class ReportParser(object):
         re['type'] = EVENT_TYPE_MAP[re['type']]
 
         ls = LocationSet()
+
         # ramen have two level location, both name location
         [ls.add(l['location']) for l in re['location']]
-
+       
         if len(ls.location_list) > 1:
             #  we need to handle this type of location list
             # split the number affect betweeen locations
@@ -49,14 +117,15 @@ class ReportParser(object):
                 re_copy = {**re}
                 re_copy['location'] = l
                 re_copy['number_affected'] = \
-                    ceil(re['number_affected'] / len(ls.location_list))
+                    ceil(re['number-affected'] / len(ls.location_list))
                 tmp_list.append(re_copy)
+
             return tmp_list
 
-            # print("Dump the error")
-            # print(re['location'])
-            # print(re['number_affected'])
-            # dd(ls.location_list)
+            print("Dump the error")
+            print(re['location'])
+            print(re['number_affected'])
+            dd(ls.location_list)
         elif ls.location_list:
 
             # use the first location in the list
@@ -64,53 +133,31 @@ class ReportParser(object):
             re['location'] = ls.location_list[0]
         else:
             re['location'] = None
-        # dd(re)
+       # dd("last call " + re)
         return [re]
 
     def dumps(self):
-        print(self.report)
         return self.report
 
-
-
-
-def mk_request(page=0):
+def mk_request():
     res = requests.get(
-        'http://api.pandetrack.online/listall/reports', params={
-            'start_date': '1900-01-01T00:00:00',
-            'end_date': '2020-01-01T00:00:00',
-            'skip': page * 50,
-            'limit':  50
-        }
+        'http://api.pandetrack.online/listall/reports',
     )
 
     res.raise_for_status()
-
+    data = res.json()
     # dd(res.json())
     # exit(0)
+   # print(data["articles"])
+    for o in data["articles"]:
 
+        o['main_text'] = o['main_text'].replace('\n', '')
+        o['report'] = [ReportParser(report).dumps() for report in o['reports']]
 
-    for article in res.json():
-        # replace the \n to ' '
-        # 抓不出来？
-        dd(article['articles'])
-        exit(0)
+        del o['reports']
 
-        article['main_text'] = article['main_text'].replace('\n', ' ')
-        article['report'] =\
-            [ReportParser(report).dumps() for report in article['reports']]
-
-        del article['reports']
-
-        print(dumps(article))
-
-    if len(res.json()) != 50:
-        exit()
+        print(dumps(o))
 
 
 if __name__ == "__main__":
-    mk_request(1)
-    # i = 0
-    # while True:
-    #     mk_request(i)
-    #     i += 1
+    mk_request()
